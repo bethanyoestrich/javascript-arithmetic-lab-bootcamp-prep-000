3bd2fc6177640745d1051af66508f412eb1b6456
Bethany Oestrich <bethany@oestrich.org>
2018-04-03 01:10:12 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,43 @@
+
+function add(a,b) {
+  return (a + b)
+}
+
+function subtract(a,b){
+  return (a-b)
+}
+
+function multiply(a,b){
+  return (a*b)
+}
+
+function divide(a,b){
+  return (a/b)
+}
+
+function inc(n){
+  return ++n 
+}
+
+function dec(n){
+  return --n
+}
+
+function makeInt(n){
+  return (parseInt('n',10))
+}
+function makeInt(n,10){
+  return (parseInt(n,10))
+}
+
+function makeInt(n,10){
+  return (parseInt("nonsense!", 10))
+}
+
+function preserveDecimal(n,10){
+  return(parseFloat (n))
+}
+
+function preserveDecimal(n,10){
+  return(parseFloat ('cats'))
+}
